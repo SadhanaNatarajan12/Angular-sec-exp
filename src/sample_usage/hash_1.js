afe2f582db8ebe3566b112548c43b7d2327b5f6d
SadhanaNatarajan12 <37762558+SadhanaNatarajan12@users.noreply.github.com>
2019-03-31 23:51:24 +0530

    Update hash_1.js

--- src/sample_usage/hash_1.js
+++ src/sample_usage/hash_1.js
@@ -7,3 +7,4 @@ console.log("Hashed value = " + hash + "\n");
 
 //var hash = crypto.createHash('sha256').update(password);
 //Here the output is: Hashed value = [object Object]
+//NOTE: The password should never be hardcoded in plaintext in the source code. Here it is done for demonstration purposes.
