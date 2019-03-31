ef7308db099f438d0b76f1946c40dd1a20de1201
SadhanaNatarajan12 <37762558+SadhanaNatarajan12@users.noreply.github.com>
2019-03-31 23:51:55 +0530

    Update jwt-hs256.js

--- src/sample_usage/jwt-hs256.js
+++ src/sample_usage/jwt-hs256.js
@@ -7,3 +7,4 @@ var newToken = jwt.sign(payload, secretKey, {
     algorithm: 'HS256'
 });
 console.log("JWT created:", newToken);
+//NOTE: The secretkey should never be hardcoded in plaintext in the source code. Here it is done for demonstration purposes.
