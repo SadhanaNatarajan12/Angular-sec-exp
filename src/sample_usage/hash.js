aa47a92ffd6576341bb3d9b41f161cc1cda9d63e
SadhanaNatarajan12 <37762558+SadhanaNatarajan12@users.noreply.github.com>
2019-03-31 23:50:59 +0530

    Update hash.js

--- src/sample_usage/hash.js
+++ src/sample_usage/hash.js
@@ -4,3 +4,5 @@ var password = '111111';
 var hash = crypto.createHash('sha256').update(password).digest('hex');
 
 console.log("Hashed value = " + hash + "\n");
+
+//NOTE: The password should never be hardcoded in plaintext in the source code. Here it is done for demonstration purposes.
